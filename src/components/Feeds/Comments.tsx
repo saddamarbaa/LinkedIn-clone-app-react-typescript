3f3f039aa5f40f4d4fa1ae6d0ec4bb60451cf435
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2025-01-25 00:15:58 -0800

    fix: enable overflow scroll on hover for comments section

--- src/components/Feeds/Comments.tsx
+++ src/components/Feeds/Comments.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Comments({ comments }: Props) {
 	return (
 		<div
-			className="my-2 mt-5 max-h-52 space-y-5 overflow-y-scroll border-t border-gray-100 p-5 px-8 dark:border-gray-800
+			className="my-2 mt-5 max-h-60 space-y-5 hover:overflow-y-scroll border-t border-gray-100 p-5 px-8 dark:border-gray-800
  ">
 			{comments.map((comment, index) => (
 				<Comment
